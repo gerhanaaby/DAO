404e8aa12086543a28e510f1816624d6a574a7fc
syafriesya <syafriwe@gmail.com>
2022-06-28 15:58:53 +0700

    env

--- Utils/Utils.go
+++ Utils/Utils.go
@@ -1,1 +1,35 @@
 package utils
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port       string
+	Hostdb     string
+	Portdb     string
+	Userdb     string
+	Passworddb string
+	Dbname     string
+}
+
+var CallConfig *Config
+
+func LoadConfig() {
+	err := godotenv.Load("./resources/.env")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	CallConfig = &Config{
+		Port:       os.Getenv("PORT"),
+		Hostdb:     os.Getenv("HOSTDB"),
+		Portdb:     os.Getenv("PORTDB"),
+		Userdb:     os.Getenv("USERDB"),
+		Passworddb: os.Getenv("PASSWORDDB"),
+		Dbname:     os.Getenv("DBNAME"),
+	}
+}
