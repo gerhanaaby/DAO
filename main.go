69b88cd861aa869e315afbb052faf4c68518384c
DESKTOP-O36QPTH\HP <gerhanaabiwow@gmail.com>
2022-06-28 17:24:14 +0700

    enak

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"DAO/database"
-	"DAO/utils"
-	"fmt"
+	"DAO/routes"
 )
-
-func init() {
-	utils.LoadConfig()
-	database.ConnectDB()
-}
 
 func main() {
 
-	fmt.Printf("helloworld")
+	routes.Run()
 }
